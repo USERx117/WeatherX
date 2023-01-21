65988c31c20bbe518a25759e797da883405a4bc2
Florian <fgotschim@gmail.com>
2023-01-21 21:02:35 +0100

    Initial commit

---
+++ src/main/java/org/fhtw/weatherX/WeatherXApplication.java
@@ -0,0 +1,13 @@
+package org.fhtw.weatherX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherXApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fhtw/weatherX/WeatherXApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fhtw.weatherX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
